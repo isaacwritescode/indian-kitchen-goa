ee19d121f3fc05189471cf8b82ae03fe22cdb0b1
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-08-25 01:13:33 +0530

    add images to landing page

--- src/pages/landing/offerings/constants.js
+++ src/pages/landing/offerings/constants.js
@@ -3,24 +3,24 @@ export const OFFERINGS = [
     label: "Rooms",
     title: "Affordable Sanctuaries: Unveiling Our Rooms in Baga",
     desc: "Step into a world of opulence and comfort at our guesthouse in Baga, Goa. Our meticulously designed rooms are a symphony of elegance and relaxation, offering you a haven to unwind and rejuvenate.",
-    img: "/images/placeholder-rect.png"
+    img: "/images/rooms.png"
   },
   {
     label: "Hospitality",
     title: "True Goan Hospitality: Where Guests Become Family",
     desc: "At our guesthouse, we take pride in offering you an authentic taste of Goan hospitality. From the moment you step foot on our premises, you become a part of our extended family.",
-    img: "/images/placeholder-rect.png"
+    img: "/images/hospitality.png"
   },
   {
     label: "Leisure",
-    title: "Immerse Yourself in Knowledge: The Mini Library Retreat",
-    desc: "For those seeking solace in the pages of a book, our mini library is a tranquil haven waiting to be explored. Nestled within our guesthouse, this cozy corner is a treasure trove of literary wonders",
-    img: "/images/placeholder-rect.png"
+    title: "Unwind and Refresh: Poolside Bliss",
+    desc: "Dive into relaxation at our Calangute, Goa paradise. Our exquisite pool facility offers crystal clear waters, comfortable lounging, and a poolside bar for ultimate leisure. Experience serenity day and night, with top-notch safety for your peace of mind. Your getaway deserves the best – welcome to our poolside haven",
+    img: "/images/pool.png"
   },
   {
     label: "Around us",
     title: "Embrace the Goa Pulse: Events Unveiled",
     desc: "Immerse yourself in the vibrant tapestry of Goa's cultural landscape with our guide to the hottest events happening around Baga. From electrifying music festivals to bustling night markets, we bring you closer to the heart of Goa's vibrant scene.",
-    img: "/images/placeholder-rect.png"
+    img: "/images/night-life.png"
   },
 ]
